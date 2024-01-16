be8a5b76bbce602600431ca372bb4a8a0b102768
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-01-15 22:41:56 -0700

    commits being lost. Uploading main manually

---
+++ main.dart
@@ -0,0 +1,18 @@
+
+
+// Begining of Flutter Journal Application
+// Flutter is based on Dart, and utilizes object oriented programming. (Similiar to C#)
+
+
+
+
+
+
+/*
+Sources Utilized:
+- Tutorials Point: https://www.tutorialspoint.com/flutter/flutter_creating_simple_application_in_android_studio.htm
+- iampawan repo (to understand how to save files): https://github.com/iampawan/FlutterExampleApps/blob/master/main.dart
+- W3 schools (to understand how flutter functions): https://www.w3schools.blog/which-one-is-better-kotlin-vs-flutter
+
+
+*/
